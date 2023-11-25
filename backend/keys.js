fe75cd7f8634f2e62cba0126b36970d36ab498f2
Vikash Mishra <131462517+vikashmishra1234@users.noreply.github.com>
2023-11-25 19:58:12 +0530

    Add files via upload

---
+++ backend/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoUrl: 'mongodb+srv://portfoliodata123:0p9o8i7u6y5t4r3e2w1q@cluster0.njc9mz0.mongodb.net/?retryWrites=true&w=majority',
+    Jwt_secret: "faslkfocvneofu"
+
+};
